e9b2312f393ed1530916f704d40d0eb82453782e
Volodymyr <volodymyr.pechenevskyi@gmail.com>
2024-10-20 18:26:13 +0200

    create a mobile navbar

---
+++ main.js
@@ -0,0 +1,9 @@
+// Mobile menu
+const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.querySelector('.links.mobile');
+menuBtn.addEventListener('click', menuToggle);
+
+function menuToggle() {
+    navbar.classList.toggle('display');
+    console.log('did it.');
+}
